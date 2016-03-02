91d244167d257ac728b0d2fe60c4657edee6795b
Forest Handford <forest.handford@affectiva.com>
2016-03-01 19:19:28 -0500

    Emotion Enabled UnityChan

--- Assets/UnityChan/Scripts/FaceUpdate.cs
+++ Assets/UnityChan/Scripts/FaceUpdate.cs
@@ -7,7 +7,16 @@ public class FaceUpdate : MonoBehaviour
 
 	Animator anim;
 
+    Transform player;
+    PlayerEmotions playerEmotions;
+
 	public float delayWeight;
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playerEmotions = player.GetComponent<PlayerEmotions> ();
+    }
 
 	void Start ()
 	{
@@ -18,6 +27,9 @@ public class FaceUpdate : MonoBehaviour
 	{
 		foreach (var animation in animations) {
 			if (GUILayout.Button (animation.name)) {
+                Debug.Log (animation.name);
+                if ("smile1@unitychan" == animation.name)
+                    Debug.Log ("Equals");
 				anim.CrossFade (animation.name, 0);
 			}
 		}
@@ -28,12 +40,29 @@ public class FaceUpdate : MonoBehaviour
 
 	void Update ()
 	{
-
 		if (Input.GetMouseButton (0)) {
 			current = 1;
 		} else {
 			current = Mathf.Lerp (current, 0, delayWeight);
 		}
-		anim.SetLayerWeight (1, current);
+
+        anim.SetLayerWeight (1, 1);
+
+        if (playerEmotions.currentSmile > 10) {
+            anim.CrossFade ("smile1@unitychan", 0.1f);
+        } else if (playerEmotions.currentSmile > 60) {
+            anim.CrossFade ("smile2@unitychan", 0.1f);
+        } else if (playerEmotions.currentAnger > 10) {
+            anim.CrossFade ("angry1@unitychan", 0.1f);
+        } else if (playerEmotions.currentSurprise > 5) {
+            anim.CrossFade ("sap@unitychan", 0.1f);
+        } else if (playerEmotions.currentAnger > 4) {
+            anim.CrossFade ("angry1@unitychan", 0.1f);
+        } else if (playerEmotions.currentValence > 5) {
+            anim.CrossFade ("conf@unitychan", 0.1f);
+        }else if (playerEmotions.currentEyeClosure > 20) {
+            anim.CrossFade ("eye_close@unitychan", 0.1f);
+        }
+        else anim.SetLayerWeight (1, current);
 	}
 }
